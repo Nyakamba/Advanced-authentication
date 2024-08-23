8d0181913df98a750db91f0b27ed4130d452c364
Nyakamba <omwegaenock@gmail.com>
2024-08-23 13:02:59 +0300

    create user

--- backend/db/connectDb.js
+++ backend/db/connectDb.js
@@ -1,1 +1,11 @@
-racf;
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Error connction to MongoDB:", error);
+    process.exit(1);
+  }
+};
