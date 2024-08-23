6ddf8f48cb29f9b331f2942077ac008ff3c07e5a
Nyakamba <omwegaenock@gmail.com>
2024-08-23 16:18:41 +0300

    send email verification

---
+++ backend/mailtrap/email.js
@@ -0,0 +1,45 @@
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { mailtrapClient, sender } from "./mailtrap.config.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Verify your Email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    });
+    console.log("Email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending verification`, error);
+
+    throw new Error(`Error sending verification email: ${error}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "61521378-90a9-486d-93ca-2c1402112f2e",
+      template_variables: {
+        company_info_name: "Auth Company",
+        name: name,
+      },
+    });
+
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
