7c81007742f508da9ee295064c16574b10d40ef4
Nyakamba <omwegaenock@gmail.com>
2024-08-24 17:29:08 +0300

    reset password

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized no valid token provided",
+    });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    console.log("Error in verifyToken", error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+};
